3b8884ade3cdcf65c790a45946d00a9beb6ae92f
Apan Qasem <apanqasem@gmail.com>
2019-03-01 16:10:38 -0600

    fp code

---
+++ code/fp_precision.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std; 
+
+const float LOOP_BOUND = 1e+08;               // hundred million 
+const unsigned long PRINT_INTERVAL = 1e+06;   // one million
+
+int main(int argc, char *argv[]) {
+
+  float i = 0; 
+  int j = 0;
+  while (i  <= LOOP_BOUND) {
+    // print counter value every millionth iteration
+    if (j % PRINT_INTERVAL == 0)
+      cout << i << endl;
+    i++;
+    j++;
+  }
+
+  return 0;
+}
