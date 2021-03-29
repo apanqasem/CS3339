4d12a9f39aca677df7272a30de337b3a9e1e1786
Apan Qasem <apanqasem@gmail.com>
2021-03-29 11:34:10 -0500

    cache examples

--- code_samples/lec5/fp_math.cpp
+++ code_samples/lec5/fp_math.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
 
-  float x = -10e+14;
-  float y = 10e+14;
+  float x = -1e+14;
+  float y = 1e+14;
   float z = 1.00; 
 
   cout << x + y + z << endl;
---
+++ code_samples/lec9/badcache.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+#define DIMSIZE 30000
+
+int main() {
+  int **a;
+  
+  a = (int **) malloc(sizeof(int *) * DIMSIZE);
+  
+  int i, j;
+  for (i = 0; i < DIMSIZE; i++) 
+    a[i] = (int *) malloc(sizeof(int) * DIMSIZE);
+
+
+  for (j = 0; j < DIMSIZE; j++) 
+    for (i = 0; i < DIMSIZE; i++)
+      a[i][j] = i + 17;
+
+
+  return 0;
+
+}
